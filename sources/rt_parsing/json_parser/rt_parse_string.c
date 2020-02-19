64fe848734dd5f6916eb476a1a8fd0de53ecb2a3
lilili21 <emkunyavskaya@list.ru>
2020-02-19 23:14:44 +0300

    add json parser

---
+++ sources/rt_parsing/json_parser/rt_parse_string.c
@@ -0,0 +1,156 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gfoote <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/27 15:57:25 by gfoote            #+#    #+#             */
+/*   Updated: 2020/01/27 15:57:32 by gfoote           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rt.h"
+#include "rt_math_utils.h"
+
+static uint32_t	search_cl_parameters(const char *tmp_value)
+{
+	if (ft_strequ(tmp_value, "RENDER_RAYTRACE"))
+		return (RENDER_RAYTRACE);
+	else if (ft_strequ(tmp_value, "RENDER_PATHTRACE"))
+		return (RENDER_PATHTRACE);
+	if (ft_strequ(tmp_value, "RENDER_RAYMARCH"))
+		return (RENDER_RAYMARCH);
+	else if (ft_strequ(tmp_value, "RENDER_MESH"))
+		return (RENDER_MESH);
+	if (ft_strequ(tmp_value, "RENDER_BACKFACE_CULLING"))
+		return (RENDER_BACKFACE_CULLING);
+	else if (ft_strequ(tmp_value, "RENDER_OBJECTS"))
+		return (RENDER_OBJECTS);
+	if (ft_strequ(tmp_value, "RENDER_MESH_VTEXTURES"))
+		return (RENDER_MESH_VTEXTURES);
+	else if (ft_strequ(tmp_value, "RENDER_TEXTURES"))
+		return (RENDER_TEXTURES);
+	else
+		rt_raise_error(ERR_PARSING_WRONG_CL_PARAM);
+	return (-1);
+}
+
+static void		parse_cl_parameters(const char *tmp_value,
+		uint32_t *renderer_flags)
+{
+	char		**cl_parameters;
+	uint32_t	flags;
+	char		*tmp_trimd;
+	int			i;
+
+	flags = 0;
+	i = 0;
+	cl_parameters = ft_strsplit(tmp_value, '|');
+	while (cl_parameters[i])
+	{
+		tmp_trimd = ft_strtrim(cl_parameters[i]);
+		flags = search_cl_parameters(tmp_trimd) | flags;
+		free(tmp_trimd);
+		i++;
+	}
+	i = 0;
+	while (cl_parameters[i])
+	{
+		ft_strclr(cl_parameters[i]);
+		free(cl_parameters[i]);
+		i++;
+	}
+	free(cl_parameters);
+	*renderer_flags = flags;
+}
+
+static void		parse_material(t_tmp *tmp, const char *key,
+		const char *tmp_value)
+{
+	if (ft_strequ(key, "diffuse"))
+	{
+		check_duplicated(tmp->checker, DIFFUSE);
+//        tmp->diffuse = get_float3_color(hex_to_int(tmp_value));
+//        printf("diffuse = [%.3f][%.3f][%.3f], \n", tmp->diffuse.x, tmp->diffuse.y, tmp->diffuse.z);
+        tmp->diffuse = get_float3_color((int)strtol(tmp_value, NULL, 16));
+//        printf("diffuse = [%.3f][%.3f][%.3f], \n", tmp->diffuse.x, tmp->diffuse.y, tmp->diffuse.z);
+	}
+	else if (ft_strequ(key, "specular"))
+	{
+		check_duplicated(tmp->checker, SPECULAR);
+		tmp->specular = get_float3_color(hex_to_int(tmp_value));
+	}
+	else if (ft_strequ(key, "color"))
+	{
+		check_duplicated(tmp->checker, COLOR);
+		tmp->color = get_float3_color(hex_to_int(tmp_value));
+	}
+	else if (ft_strequ(key, "emission color"))
+	{
+		check_duplicated(tmp->checker, EMISSION_COLOR);
+		tmp->emission_color = get_float3_color(hex_to_int(tmp_value));
+	}
+	else if (ft_strequ(key, "texture"))
+	{
+		check_duplicated(tmp->checker, TEXTURE);
+		tmp->texture_number = parse_texture(tmp_value);
+	}
+}
+
+static void		parse_type(t_tmp *tmp, const char *value)
+{
+	if (tmp->structure_type == LIGHT)
+	{
+		if (ft_strequ(value, "ambient"))
+			tmp->type = AMBIENT;
+		else if (ft_strequ(value, "point"))
+			tmp->type = POINT;
+		else
+			rt_raise_error(ERR_PARSING_WRONG_LIGHT_PARAMS);
+	}
+	else if (tmp->structure_type == OBJECT)
+	{
+		if (ft_strequ(value, "sphere"))
+			tmp->type = SPHERE;
+		else if (ft_strequ(value, "cone"))
+			tmp->type = CONE;
+		else if (ft_strequ(value, "cylinder"))
+			tmp->type = CYLINDER;
+		else if (ft_strequ(value, "plane"))
+			tmp->type = PLANE;
+		else if (ft_strequ(value, "AABB"))
+			tmp->type = AABB;
+		else
+			rt_raise_error(ERR_PARSING_WRONG_OBJECT_PARAMS);
+	}
+	else
+		rt_raise_error(ERR_PARSING_WRONG_TYPE);
+}
+
+void			parse_string(t_tmp *tmp, const char *key, json_t *value,
+		uint32_t *renderer_flags)
+{
+	const char *tmp_value;
+
+	tmp_value = json_string_value(value);
+	if (tmp->structure_type == NOT_SET)
+	{
+		if (ft_strequ(key, "open cl parameters"))
+		{
+			if (renderer_flags == 0)
+				rt_raise_error(ERR_PARSING_DUPLICATED_PARAM);
+			parse_cl_parameters(tmp_value, renderer_flags);
+		}
+		else if (ft_strequ(key, "skybox"))
+			ft_add_texture_name_back(&g_textures.textures_names, tmp_value);
+		else
+			rt_raise_error(ERR_PARSING_WRONG_TYPE);
+	}
+	else if (tmp->type == NOT_SET && ft_strequ(key, "type"))
+		parse_type(tmp, tmp_value);
+	else if (tmp->structure_type != NOT_SET && tmp->type != NOT_SET)
+		parse_material(tmp, key, tmp_value);
+	else
+		rt_raise_error(ERR_PARSING_WRONG_TYPE);
+}
---
+++ sources/rt_parsing/json_parser/rt_type_of_parametr.c
@@ -0,0 +1,83 @@
+#include "rt.h"
+
+static int	ft_check_material(const char *param, int *type_of_element)
+{
+	if (ft_strequ(param, "ambient"))
+		*type_of_element = AMBIENCE;
+	else if (ft_strequ(param, "diffuse"))
+		*type_of_element = DIFFUSE;
+	else if (ft_strequ(param, "specular"))
+		*type_of_element = SPECULAR;
+	else if (ft_strequ(param, "emission color"))
+		*type_of_element = EMISSION_COLOR;
+	else if (ft_strequ(param, "texture position"))
+		*type_of_element = TEXTURE_POS;
+	return (*type_of_element != -1) ? 1 : -1;
+}
+
+static int	ft_check(const char *param, int *type_of_element)
+{
+	if (ft_strequ(param, "rotation"))
+		*type_of_element = ROTATION;
+	else if (ft_strequ(param, "pos"))
+		*type_of_element = POS;
+	else if (ft_strequ(param, "direction"))
+		*type_of_element = DIRECTION;
+	else if (ft_strequ(param, "color"))
+		*type_of_element = COLOR;
+	else if (ft_strequ(param, "normal"))
+		*type_of_element = NORMAL;
+	else if (ft_strequ(param, "axis"))
+		*type_of_element = AXIS;
+	else if (ft_strequ(param, "center"))
+		*type_of_element = CENTER;
+	else if (ft_strequ(param, "vmin"))
+		*type_of_element = VMIN;
+	else if (ft_strequ(param, "vmax"))
+		*type_of_element = VMAX;
+	return (*type_of_element != -1) ? 1 :
+	ft_check_material(param, type_of_element);
+}
+
+int			ft_type_of_array(int *type_of_element,
+		const char *param, int structure_type)
+{
+	if (ft_strequ(param, "objects"))
+		*type_of_element = OBJECT;
+	if (ft_strequ(param, "light"))
+		*type_of_element = LIGHT;
+	if (structure_type == LIGHT)
+	{
+		if (ft_strequ(param, "ambient"))
+			*type_of_element = AMBIENT;
+		else if (ft_strequ(param, "point"))
+			*type_of_element = POINT;
+	}
+	if (structure_type == OBJECT)
+	{
+		if (ft_strequ(param, "sphere"))
+			*type_of_element = SPHERE;
+		else if (ft_strequ(param, "cone"))
+			*type_of_element = CONE;
+		else if (ft_strequ(param, "cylinder"))
+			*type_of_element = CYLINDER;
+		else if (ft_strequ(param, "plane"))
+			*type_of_element = PLANE;
+		else if (ft_strequ(param, "AABB"))
+			*type_of_element = AABB;
+	}
+	return (*type_of_element != -1) ? 2 : (ft_check(param, type_of_element));
+}
+
+int			ft_type_of_structure(const char *param)
+{
+	if (ft_strequ(param, "object"))
+		return (OBJECT);
+	else if (ft_strequ(param, "light"))
+		return (LIGHT);
+	else if (ft_strequ(param, "camera"))
+		return (CAMERA);
+	else if (ft_strequ(param, "material"))
+		return (MATERIAL);
+	return (-1);
+}
