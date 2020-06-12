7a1f19a1f4e6be1d25471bd1175272040bdc54e6
sleonard <olegkun3012@gmail.com>
2020-06-12 14:04:33 +0300

    move external headers to 'external' directory

--- sources/debug/rt_debug_print_utils.c
+++ sources/debug/rt_debug_print_utils.c
@@ -11,19 +11,3 @@ void		rt_print_float3_color(cl_float3 color, const char *definition)
 	printf("%s: r: [%.2f], g: [%.2f], arr: [%.2f]\n",
 			definition, color.x, color.y, color.z);
 }
-
-void		rt_print_material(const t_material *material)
-{
-	ft_printf("material:\n");
-	rt_print_float3_color(material->diffuse, "diffuse");
-	rt_print_float3_color(material->specular, "specular");
-	printf("smoothness: [%.2f]\n", material->smoothness);
-	printf("phong exponent: [%.2f]\n", material->phong_exp);
-	printf("refraction: [%.2f]\n", material->refraction);
-	printf("transmittance: [%.2f]\n", material->transmittance);
-	if (material->emission_power > 0)
-	{
-		printf("emission_power: [%f]\n", material->emission_power);
-		rt_print_float3_color(material->emission_color, "emission color");
-	}
-}
