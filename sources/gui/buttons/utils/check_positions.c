4e7fe0489cdea2a8f5253fc0cb31f6749263ade8
Dyan Great <dgreat@oa-l1.msk.21-school.ru>
2020-07-09 18:34:01 +0300

    Final

--- sources/gui/buttons/utils/check_positions.c
+++ sources/gui/buttons/utils/check_positions.c
@@ -6,7 +6,7 @@
 /*   By: gfoote <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/12 12:07:40 by gfoote            #+#    #+#             */
-/*   Updated: 2020/06/12 12:07:46 by gfoote           ###   ########.fr       */
+/*   Updated: 2020/07/09 17:46:33 by dgreat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ bool	is_positions_changed(int tb_id, float curr_value)
 {
 	if (g_gui.obj[tb_id].param != curr_value)
 	{
+		free(g_gui.obj[tb_id].field);
 		ft_sprintf(&(g_gui.obj[tb_id].field), "%i", (int)(curr_value));
 		g_gui.obj[tb_id].param = curr_value;
 		return (true);
